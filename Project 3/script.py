8920d3f5e6577c5321411b00fa8e3895ca99927d
hayimheron <102389144+hayimheron@users.noreply.github.com>
2022-08-04 10:29:43 -0400

    Add files via upload

---
+++ Project 3/script.py
@@ -0,0 +1,17 @@
+2+2
+'Hello'
+print(2+2)
+print('Hello')
+x=2+2+2+2
+print(x)
+
+x,y=3,4
+print(x,y)
+
+import datetime
+
+print(datetime.datetime.now())
+print('Peter'.upper())
+print('Current time is:',datetime.datetime.now())
+#print('Current time is:'+ datetime.datetime.now())
+print('Hello'+'Again')
